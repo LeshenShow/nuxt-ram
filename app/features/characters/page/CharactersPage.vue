2596c6e07252cd54ffffdd2239aaa3baef930bac
LeshenShow <leshenshow@gmail.com>
2025-11-26 20:20:21 +0300

    add skeleton for cards

--- app/features/characters/page/CharactersPage.vue
+++ app/features/characters/page/CharactersPage.vue
@@ -6,11 +6,12 @@
         <UButton color="primary" size="sm">Favorites</UButton>
       </div>
     </UHeader>
-
+<!-- v-if="store.loading" -->
     <UMain>
       <UContainer class="py-6">
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-          <CharacterCard v-for="char in store.characters" :key="char.id" :character="char" @edit="openNotesModal" />
+           <CharacterCardSkeleton    v-if="delayedLoading" v-for="i in 8" :key="'skeleton-'+i" />
+          <CharacterCard v-if="!delayedLoading" v-for="char in store.characters" :key="char.id" :character="char" @edit="openNotesModal" />
         </div>
       </UContainer>
     </UMain>
@@ -25,18 +26,20 @@ import CharacterCard from "~/features/characters/ui/CharacterCard.vue"
 import EditNotesModal from "~/features/characters/ui/modal/EditNotesModal.vue"
 import { useCharactersStore } from "~/features/characters/model/charactersStore"
 import type { Character } from "../model"
+import CharacterCardSkeleton from "../ui/CharacterCardSkeleton.vue"
 
 const store = useCharactersStore()
 const editingCharacter = ref<Character | null>(null)
 const editingNotes = ref("")
 const showModal = ref(false)
-
+const delayedLoading = ref(true)
 // Открываем модалку с выбранным персонажем
 const openNotesModal = (char: Character) => {
   editingCharacter.value = char
   editingNotes.value = char.notes || ""
   showModal.value = true
 }
+
 
 // Сохраняем заметки через store
 const saveNotes = (notes: string) => {
@@ -48,5 +51,14 @@ const saveNotes = (notes: string) => {
 
 onMounted(() => {
   store.fetchCharacters(1)
+})
+watch(() => store.loading, (isLoading) => {
+  if (isLoading) {
+    delayedLoading.value = true
+  } else {
+    setTimeout(() => {
+      delayedLoading.value = false
+    }, 300) // ← задержка исчезновения (мс)
+  }
 })
 </script>
